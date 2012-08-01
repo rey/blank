647a3e1dc617d39dfd2133e4246750901014863e
Rey Dhuny <rey@me.com>
2012-08-01 17:17:47 +0100

    adding various bits

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "stylesheets/css/"
+sass_dir = "stylesheets/sass/"
+
+output_style = :expanded
+
+relative_assets = true
+
+line_comments = false
+
+preferred_syntax = :sass
