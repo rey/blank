1da5e9e749d3ec9ec15b8c76a7d84f5bd004a91f
Rey Dhuny <rey@me.com>
2012-10-09 15:10:57 +0100

    adding image path stuff

--- config.rb
+++ config.rb
@@ -3,13 +3,13 @@ require "compass-respond"
 http_path = "/"
 css_dir = "assets/stylesheets/css/"
 sass_dir = "assets/stylesheets/sass/"
+images_dir = "assets/images/"
 fonts_dir = "assets/fonts/"
 
-output_style = :compact
+output_style = :expanded
 
 relative_assets = true
 
 line_comments = false
 
 preferred_syntax = :sass
-
