a40fecfa0cd8a3325872ef371feea0c3236cffc5
Rey Dhuny <rey@me.com>
2012-10-09 14:37:31 +0100

    adding fonts stuff

--- config.rb
+++ config.rb
@@ -3,11 +3,13 @@ require "compass-respond"
 http_path = "/"
 css_dir = "assets/stylesheets/css/"
 sass_dir = "assets/stylesheets/sass/"
+fonts_dir = "assets/fonts/"
 
-output_style = :compressed
+output_style = :compact
 
 relative_assets = true
 
 line_comments = false
 
 preferred_syntax = :sass
+
